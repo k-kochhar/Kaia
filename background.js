c74b4c6504f030b5bcae1b56e3900d450f7f1dc3
Kshitij Kochhar <kshitij@ctfguide.com>
2024-12-21 18:57:46 -0500

    [test] Testing contentScript + background

--- background.js
+++ background.js
@@ -1,1 +1,5 @@
-// Hello World
+console.log("Background script is running!");
+
+chrome.runtime.onInstalled.addListener(() => {
+  console.log("Kaia installed or updated!");
+});
--- contentScript.js
+++ contentScript.js
@@ -1,2 +1,11 @@
-// Hello World
-console.log("Hello World");
+function getArticleText() {
+  const paragraphs = document.querySelectorAll('p');
+  let text = "";
+  paragraphs.forEach((p) => {
+    text += p.innerText + "\n";
+  });
+  return text;
+}
+
+const articleText = getArticleText();
+console.log("Extracted article text:", articleText);

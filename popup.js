ca2c01d56aa0e333a841a59d140d24a60e1526c9
Kshitij Kochhar <kshitij@ctfguide.com>
2024-12-21 18:50:39 -0500

    Initial commit

---
+++ popup.js
@@ -0,0 +1,7 @@
+// popup.js
+console.log("Popup script loaded!");
+
+// If you want to do something when the popup loads:
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Popup DOM loaded!");
+});
